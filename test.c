ed46408e1ac531e18489a453313522c5280b9ca5
wuuyi <38210249+wy3@users.noreply.github.com>
2019-01-23 16:18:15 +0700

    update test.c

--- test.c
+++ test.c
@@ -1,13 +1,13 @@
 #include "wobj.h"
 
-// declare Dog object (currently, `Dog` is a struct)
-wobj(Dog) {
+// declare Dog object (use wobj_np for no private)
+wobj_np(Dog, {
 
-	uint32_t weight; // property
-	char *name;
-	void(*speak)(); // method
+    uint32_t weight; // property
+    char *name;
+    wobj_fn(void, speak, ()); // method
 
-} /*, you can define type here as struct*/;
+} /*; you can declare new type here as Dog */);
 
 // define 'speak' method, like: void Dog.speak(void) 
 wobj_def(Dog, void, speak, (void), {
@@ -17,29 +17,28 @@ wobj_def(Dog, void, speak, (void), {
 
 // init Dog
 wobj_init(Dog, (const char* name, uint32_t weight), {
-	// set method
+    // set method
     wobj_set(Dog, speak); // set 'sepak' method
 
     // set property
-	self->weight = weight;
+    self->weight = weight;
 
-	size_t len = strlen(name) + 1;
-	// allocate by `wobj_*alloc*`
-	self->name = wobj_alloc(Dog, len); // if want to free, must use wobj_unalloc(), dont use free()
-	memcpy(self->name, name, len); // copy
+    size_t len = strlen(name);
+    // allocate by `wobj_*alloc*`
+    self->name = wobj_alloc(len + 1); // if want to free, must use wobj_unalloc(), do not use free()
+    memcpy(self->name, name, len); // copy
 }, {
-	// on free, called by wobj_free()  
+    // on free, called by wobj_free()  
 })
 
 int main(void) {
-	// create mydog as Dog object
-	wobj_new(Dog, dog_foo, "Crazy Dog!", 32);
-	// call method
-	dog_foo->speak();
-	// << I'm Crazy Dog!, my weight is 32kg.
+    // create mydog as Dog object
+    wobj_new(Dog, dog_foo, "Crazy Dog!", 32);
+    // call method
+    dog_foo->speak();
+    // << I'm Crazy Dog!, my weight is 32kg.
+    wobj_free(Dog, dog_foo); // free dog_foo
 
-	wobj_free(Dog, dog_foo); // free dog_foo
-
-	puts("done!\n");
-	return 0;
+    puts("done!\n");
+    return 0;
 }
