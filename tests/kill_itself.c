f2a82cbff58ee2b20ce56e1349c73dff9f27261c
wuuyi <38210249+wy3@users.noreply.github.com>
2019-02-03 09:53:34 +0700

    create kill_itself example

---
+++ tests/kill_itself.c
@@ -0,0 +1,49 @@
+#include "wobj.h"
+
+wobj(Test
+    {
+        const int value;
+        
+        wobj_fn(void*, get, ());
+        wobj_fn(void, kill, ());
+    },
+    {
+        void *reserved;
+    }
+)
+
+wobj_def(Test, void*, get, (),
+{
+    return self->reserved;
+})
+
+wobj_def(Test, void, kill, (),
+{
+    puts("kill itself");
+    wobj_free(Test, self);
+})
+
+wobj_init(Test, (void),
+{
+    wobj_set(Test, get);
+    wobj_set(Test, kill);
+    
+    *(int*)&self->value = rand();
+    self->reserved = malloc(sizeof(32));
+},
+{
+    free(self->reserved);
+    puts("freeing");
+})
+
+int main() {
+
+    Test it = wobj_new4(Test)();
+
+    printf("%d", it->value)
+    it->get();
+    it->kill()
+    
+    puts("done!");
+    return 0;
+}
