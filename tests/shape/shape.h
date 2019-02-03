502e2ae5b514a9216cb6df9c6c2c6d913e5592d3
Meouu <38274907+linhuyen@users.noreply.github.com>
2019-02-03 10:01:36 +0700

    update

--- tests/shape/shape.h
+++ tests/shape/shape.h
@@ -3,24 +3,24 @@
 // tested on Windows, but many names same to function name
 
 wobj_np(_Shape,
-{
-    wobj_fn (void, print_area, (void));
-
-});
+    {
+        wobj_fn (void, print_area, (void));
+    }
+);
 
 wobj_np(_Rectangle,
-{
-    wobj_public(_Shape);
-    
-    int width;
-    int height;
+    {
+        wobj_public(_Shape);
 
-});
+        int width;
+        int height;
+    }
+);
 
 wobj_np(_Circle,
-{
-    wobj_public(_Shape);
+    {
+        wobj_public(_Shape);
 
-    int radius;
-
-});
+        int radius;
+    }
+);
