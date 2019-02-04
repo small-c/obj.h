a89e46637de6c6df285e187974bfc85050371bfe
wuuyi <wuuyi123@gmail.com>
2019-02-05 01:35:15 +0700

    update

--- tests/animal/animal.h
+++ tests/animal/animal.h
@@ -1,17 +1,15 @@
 #include "wobj.h"
 
-wobj(Animal, {
-// public:
-    char    *name;
-    int     weight;
-
-    wobj_fn(void, speak, (void));
-
-}, {
-// private:
-    int     power;
-
-});
+wobj(Animal,
+    public (
+        char   *name;
+		int    weight;
+        void   func(speak, ());
+    ),
+    private (
+        int    power;
+    )
+);
 
 Animal newAnimal(const char *name, int weight);
 void freeAnimal(Animal animal);
