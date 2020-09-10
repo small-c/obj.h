e2ec7d9cfb044939cf9c4ae9d7e31d530484676a
nomi-san <wuuyi123@gmail.com>
2020-09-10 16:49:23 +0700

    Add xmake build script

---
+++ xmake.lua
@@ -0,0 +1,22 @@
+
+set_kind("binary")
+add_rules("mode.release")
+add_includedirs("./")
+
+if is_os("windows") then
+    target("win32_app")
+        add_files("tests/win32_app/*.c")
+        add_links("kernel32", "user32")
+end
+
+target("foo")
+    add_files("tests/foo.c")
+
+target("dog")
+    add_files("tests/dog.c")
+
+target("animal")
+    add_files("tests/animal/*.c")
+
+target("shape")
+    add_files("tests/shape/*.c")
